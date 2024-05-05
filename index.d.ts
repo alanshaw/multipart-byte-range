256cb8037ddccfcbb566c6bc197f2a2eb1a300aa
Alan Shaw <alan.shaw@protocol.ai>
2024-05-05 15:12:59 +0200

    docs: add docs for range types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,34 @@
+/**
+ * An absolute byte range to extract - always an array of two values
+ * corresponding to the first and last bytes (both inclusive). e.g.
+ * 
+ * ```
+ * [100, 200]
+ * ```
+ */
 export type AbsRange = [first: number, last: number]
+
+/**
+ * Byte range to extract - an array of one or two values corresponding to the
+ * first and last bytes (both inclusive). e.g.
+ * 
+ * ```
+ * [100, 200]
+ * ```
+ * 
+ * Omitting the second value requests all remaining bytes of the resource. e.g.
+ * 
+ * ```
+ * [900]
+ * ```
+ * 
+ * Alternatively, if it's unknown how large a resource is, the last `n` bytes
+ * can be requested by specifying a negative value:
+ * 
+ * ```
+ * [-100]
+ * ```
+ */
 export type Range = AbsRange | [first: number]
 
 export type RangeGetter = (range: AbsRange) => Promise<Uint8Array>
